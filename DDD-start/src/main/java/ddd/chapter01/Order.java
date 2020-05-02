362ba0b2b3960b1815d79f94b18ff1ddf5389218
ljh3259 <ljh3259@naver.com>
2020-05-02 21:33:02 +0900

    Order 객체를 정의하라
    * Order 가 제공해야할 기능에 대한 메서드 시그니처를 정의했습니다

---
+++ DDD-start/src/main/java/ddd/chapter01/Order.java
@@ -0,0 +1,17 @@
+package ddd.chapter01;
+
+public class Order {
+
+  public void changeShipped() {
+  }
+
+  public void changeShippingInfo(ShippingInfo ne써Shipping) {
+  }
+
+  public void cancel() {
+  }
+
+  public void completePayment() {
+  }
+
+}
