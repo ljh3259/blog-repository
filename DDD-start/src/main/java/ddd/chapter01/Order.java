a0209c63e2b88977b58a61e84e3c926dcb37204b
ljh3259 <ljh3259@naver.com>
2020-05-02 21:50:03 +0900

    주문의 최소주문 제약조건과 총구매금액 메서드를 추가하라
    * 총 구매금액 메서드의 구현체가 완료되지 않아 컴파일 에러가 발생하고있다

--- DDD-start/src/main/java/ddd/chapter01/Order.java
+++ DDD-start/src/main/java/ddd/chapter01/Order.java
@@ -1,6 +1,36 @@
 package ddd.chapter01;
 
+import java.util.List;
+
 public class Order {
+
+  private List<OrderLine> orderLines;
+  private int totalAmounts;
+
+  public Order(List<OrderLine> orderLines) {
+    setOrderLines(orderLines);
+  }
+
+  private void setOrderLines(List<OrderLine> orderLines) {
+    verifyAtLeastOneOrMoreOrderLines(orderLines);
+    this.orderLines = orderLines;
+    calculateTotalAmounts();
+  }
+
+
+  private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+    if (orderLines == null || orderLines.isEmpty()) {
+      throw new IllegalArgumentException("no OrderLine");
+    }
+  }
+
+  private void calculateTotalAmounts() {
+    this.totalAmounts = new Money(
+        orderLines.stream()
+            .mapToInt(x -> x.getAmounts().getValue())
+            .sum()
+    );
+  }
 
   public void changeShipped() {
   }
