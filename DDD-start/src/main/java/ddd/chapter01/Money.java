cf9b36f4330575be7f52145331e54fb2c59e219c
ljh3259 <ljh3259@naver.com>
2020-05-02 21:50:03 +0900

    주문의 최소주문 제약조건과 총구매금액 메서드를 추가하라
    * 총 구매금액 메서드의 구현체가 완료되지 않아 컴파일 에러가 발생하고있다

---
+++ DDD-start/src/main/java/ddd/chapter01/Money.java
@@ -0,0 +1,6 @@
+package ddd.chapter01;
+
+public class Money {
+  public Money(int sum) {
+  }
+}
--- DDD-start/src/main/java/ddd/chapter01/OrderLine.java
+++ DDD-start/src/main/java/ddd/chapter01/OrderLine.java
@@ -14,9 +14,15 @@ public class OrderLine {
     this.amounts = amounts;
   }
 
+  /**
+   * 구매가격을 계산한다.
+   *
+   * @return 개당 상품가격 * 구매 수량
+   */
   private int calculateAmounts() {
     return price * quantity;
   }
+
 
   public int getAmounts() {
     return 0;
