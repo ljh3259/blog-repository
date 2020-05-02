b3ecf04c3ad48b33ba29703493be13616e63881c
ljh3259 <ljh3259@naver.com>
2020-05-02 22:46:44 +0900

    Order 객체의 equals, hashCode 메서드를 구현하라

--- DDD-start/src/main/java/ddd/chapter01/Order.java
+++ DDD-start/src/main/java/ddd/chapter01/Order.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class Order {
 
+  private String orderNumber;
   private List<OrderLine> orderLines;
   private Money totalAmounts;
   private ShippingInfo shippingInfo;
@@ -65,4 +66,32 @@ public class Order {
   public void completePayment() {
   }
 
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null) {
+      return false;
+    }
+
+    if (obj.getClass() != Order.class) {
+      return false;
+    }
+
+    Order other = (Order) obj;
+    if (this.orderNumber == null) {
+      return false;
+    }
+    return this.orderNumber.equals(other.orderNumber);
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((orderNumber == null) ? 0 : orderNumber.hashCode());
+    return result;
+  }
 }
