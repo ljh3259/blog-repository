492448136b4817b76c2610632ce490d9f972e342
ljh3259 <ljh3259@naver.com>
2020-05-05 12:56:06 +0900

    int 타입 필드를 돈을 의미하는 Money 밸류타입으로 변경하라
    * price, amouts 를 변경했습니다.
    * 구매가격을 계산 하는 calculateAmounts() 생성자에서 호출하도록 변경했습니다.

--- DDD-start/src/main/java/ddd/chapter01/OrderLine.java
+++ DDD-start/src/main/java/ddd/chapter01/OrderLine.java
@@ -3,15 +3,15 @@ package ddd.chapter01;
 public class OrderLine {
 
   private Product product;
-  private int price;
+  private Money price;
   private int quantity;
-  private int amounts;
+  private Money amounts;
 
-  public OrderLine(Product product, int price, int quantity, int amounts) {
+  public OrderLine(Product product, Money price, int quantity) {
     this.product = product;
     this.price = price;
     this.quantity = quantity;
-    this.amounts = amounts;
+    this.amounts = this.calculateAmounts();
   }
 
   /**
@@ -19,14 +19,13 @@ public class OrderLine {
    *
    * @return 개당 상품가격 * 구매 수량
    */
-  private int calculateAmounts() {
-    return price * quantity;
+  private Money calculateAmounts() {
+    return price.multiply(quantity);
   }
 
 
   public int getAmounts() {
     return 0;
   }
-
 
 }
